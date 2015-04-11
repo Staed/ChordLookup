5b34b3a7b3c680b432018be7c48624120a1321be
Kelsey Yuen <heavenslashinghell@gmail.com>
2015-04-11 12:25:38 -0500

    Wrote some of coordinator logic

--- chordlookup.py
+++ chordlookup.py
@@ -1,30 +1,40 @@
 # CS425 MP2
 import socket
 import threading
-import sys
 from threading import Thread
+import sys
+from sys import stdin
 
 identifier = 0
 keys[256]
+threads = []
 
 class chordlookup(object):
     def __init__(self, input):
         self._Queue= Queue.Queue()
 
     def start(self):
-        self.t_listen=threading.Thread(target=self.listen)
-        self.t_listen.start()
-        self.t_send=threading.Thread(target=self.send)
-        self.t_send.start()
-        
-    def listen(self):   # Coordinator Thread
-        while True:
-            message, addr = self.s_listen.recvfrom(1024)
-                
-    def send(self):
-        while True:
-            message = self._Queue.get()
+        self.t_coord=threading.Thread(target=self.coordinator)
+        self.t_coord.start()
 
+        for thread in threads:
+            thread.join()
+        self.t_coord.join()
+        
+    def coordinator(self):   # Coordinator Thread
+        exitFlag = False
+        while not(exitFlag):
+            # message, addr = self.s_listen.recvfrom(1024)
+            userinput = stdin.readline()
+            cmdP = userInput.split(" ")
+
+            if cmdP[0] == "join":
+                thread = threading.Thread(target=node)
+                thread.start()
+
+    def node(self):
+        # Stuff
+                
 if __name__ == '__main__':
     if len(sys.argv) <2:
         sys.argv.append(raw_input('Central Server> Please choose the consistency model:\n 1.Linearizability\n 2.Sequential consistency\n 3.Eventual consistency, W=1, R=1\n 4.Eventual consistency, W=2, R=2\n Central Server>'))
