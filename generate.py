a743e042b975703a0f74e219b9b59b05f971167a
Kelsey Yuen <heavenslashinghell@gmail.com>
2015-04-19 18:32:24 -0500

    Generator node number pass in as argument

--- generate.py
+++ generate.py
@@ -8,19 +8,19 @@ def main(argv):
 
     # Add P nodes without replacement, P = 4, 8, 10, 20, 30, and others
     # Message
-    population = [4, 8, 10, 20, 30]
+    population = []#[4, 8, 10, 20, 30]
     sampleNodes = range(1, 256)
-    sampleNodes.remove(4)
-    sampleNodes.remove(8)
-    sampleNodes.remove(10)
-    sampleNodes.remove(20)
-    sampleNodes.remove(30)
+    #sampleNodes.remove(4)
+    #sampleNodes.remove(8)
+    #sampleNodes.remove(10)
+    #sampleNodes.remove(20)
+    #sampleNodes.remove(30)
 
     for i in population:
         cmdFile.write("join " + str(i) + "\n")
         cmdFile.write("wait 0.5\n")
 
-    for i in range(1, 11):
+    for i in range(1, int(argv[1])+1):
         random.shuffle(sampleNodes)
         curNode = sampleNodes[0:1]
         population.append(curNode[0])
@@ -30,12 +30,12 @@ def main(argv):
 
     cmdFile.write("wait 2\n")
     cmdFile.write("message\n")
-    cmdFile.write("wait 0.5\n")
+    cmdFile.write("wait 5\n")
     
     # Find >= 64 times, with p & k randomly selected... p from the P set
     # Message / Find times
 
-    for i in range(1, 100):
+    for i in range(1, 101):
         findNode = random.sample(population, 1)
         cmdFile.write("find " + str(findNode[0]) + " " + str(random.randint(0, 255)) + "\n")
         cmdFile.write("wait 0.5\n")
